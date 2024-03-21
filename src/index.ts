b7865cb58b45369c52109ea349fed761e43faa9d
holybaechu <holyb@holyb.xyz>
2024-03-21 21:59:15 +0900

    Bump Version, Update Exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export { Client } from "./client/Client";
 export { GatewayIntentBits } from "./gateway/GatewayIntentBits";
 export { Events } from "./gateway/Events";
+
 export { ShardingManager } from "./sharding/ShardingManager";
+export { ShardClientUtil } from "./sharding/ShardClientUtil";
+export { Shard } from "./sharding/Shard";
