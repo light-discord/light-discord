2ed88f1e30d3c17170a477225e46f8de5f4d732b
RappyTV <rappytv@rappytv.com>
2024-03-23 17:20:25 +0100

    Document Client and ClientOptions

--- src/client/ClientOptions.ts
+++ src/client/ClientOptions.ts
@@ -1,5 +1,15 @@
 import { GatewayIntentBits } from '../gateway/GatewayIntentBits';
 
 export interface ClientOptions {
+    /**
+     * An array of the bot's intents you need
+     * @example
+     * intents: [
+     *    GatewayIntentBits.Guilds,
+     *    GatewayIntentBits.GuildMembers,
+     *    GatewayIntentBits.GuildMessages,
+     *    GatewayIntentBits.MessageContent
+     * ]
+     */
     intents: GatewayIntentBits[];
 }
