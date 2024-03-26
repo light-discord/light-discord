9c630d8f9d884d0a642b7524f8b4d3f18d95da1a
holybaechu <holyb@holyb.xyz>
2024-03-26 22:27:18 +0900

    Bun fixed the bug

--- src/client/events/EventsManager.ts
+++ src/client/events/EventsManager.ts
@@ -12,8 +12,8 @@ export class EventsManager {
         })();
     }
 
-    register(name: string, handler: { [ key: string ]: any }) { // there is a bun.js(or tsup idk) bug where i need to use 2 default variables
-        this.events[name] = handler.default.default || handler.default;
+    register(name: string, handler: { default: Function }) {
+        this.events[name] = handler.default;
     }
 }
 
